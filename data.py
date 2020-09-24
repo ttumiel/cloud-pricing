ea0aed5b533396e14ff22c68e4845b0d58533d2f
Thomas Tumiel <ttumiel@gmail.com>
2020-09-24 15:33:56 +0200

    add fixed and custom instance filtering

--- data.py
+++ data.py
@@ -36,6 +36,9 @@ class DataProcessor:
                 for chunk in tqdm(r.iter_content(chunk_size=8192), total=l):
                     f.write(chunk)
 
+
+class FixedInstance(DataProcessor):
+    "Filter from a table of predefined instances"
     def filter(self, cpus, ram, gpus=0, gpuram=10, n=10, verbose=False, include_unk_price=False):
         df = self.table.copy()
         if not verbose:
@@ -49,8 +52,51 @@ class DataProcessor:
 
         return df.sort_values('Price ($/hr)')[:n]
 
+class CustomInstance(DataProcessor):
+    """Process instances that can be customized on demand
+    by selecting the cpus, gpus, etc. and multiplying by
+    the cost per unit.
 
-class AWSProcessor(DataProcessor):
+    Should setup self.cpu_pricing and self.gpu_pricing
+    """
+    def filter(self, cpus, ram, gpus=0, gpuram=10, verbose=False):
+        if ram//cpus > 8:
+            print("More than 8GB of RAM per CPU may lead to additional costs.")
+
+        # Handle CPUs and RAM
+        df = self.cpu_pricing.copy()
+        df['CPU Price ($/hr)'] = df['Price ($/hr)'] * cpus
+        df['RAM ($/hr)'] = df['RAM ($/hr)'] * ram
+        df.insert(0, 'CPUs', [cpus]*len(df))
+        df.insert(0, 'RAM (GB)', [ram]*len(df))
+        df['Price ($/hr)'] = df['CPU Price ($/hr)'] + df['RAM ($/hr)']
+
+        if gpus>0:
+            # Handle GPUs and GPU RAM
+            gpus_df = self.gpu_pricing.copy()
+            counts = [min(max(gpus, math.ceil(gpuram/r)),m)
+                     for r,m in zip(gpus_df['GPU RAM (GB)'], gpus_df['Max #'])]
+            gpus_df.insert(0, 'GPUs', counts)
+            gpus_df['GPU Price ($/hr)'] = gpus_df['GPU Price ($/hr)'] * counts
+            gpus_df['GPU RAM (GB)'] = gpus_df['GPU RAM (GB)'] * counts
+
+            # For every CPU/RAM combination, attach the required GPUs
+            df = pd.concat([
+                pd.DataFrame([
+                    pd.concat([df.iloc[i], gpus_df.iloc[j]])
+                    for i in range(len(df))
+                ], columns=df.columns.append(gpus_df.columns))
+                for j in range(len(gpus_df))
+            ])
+
+            # Update total price
+            df['Price ($/hr)'] = df['Price ($/hr)'] + df['GPU Price ($/hr)']
+
+        return df.filter(['Name', 'Type', 'CPUs', 'RAM (GB)']+
+                         (['GPUs', 'GPU RAM (GB)'] if gpus>0 else [])+['Price ($/hr)']).sort_values('Price ($/hr)')
+
+
+class AWSProcessor(FixedInstance):
     aws_gpu_ram = {
         'p3': ('V100', 16),
         'p2': ('K80', 12),
@@ -134,6 +180,9 @@ class AWSProcessor(DataProcessor):
 
         # Save data
         combined.to_pickle(self.table_name)
+
+        os.remove(data_name)
+
 
 class AzureProcessor(FixedInstance):
     url = 'https://azure.microsoft.com/en-us/pricing/details/virtual-machines/linux/'
